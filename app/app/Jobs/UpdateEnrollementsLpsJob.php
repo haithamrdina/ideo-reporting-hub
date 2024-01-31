d922265ead90039c0e9070a7bbd848464c057418
haithamrdina <hridna@ideolearning.com>
2024-01-31 14:57:33 +0100

    update job function for lp  enrollments by adding 4 times (session/cmi/calculated/recommended)

--- app/app/Jobs/UpdateEnrollementsLpsJob.php
+++ app/app/Jobs/UpdateEnrollementsLpsJob.php
@@ -4,17 +4,14 @@ namespace App\Jobs;
 
 use App\Http\Integrations\Docebo\DoceboConnector;
 use App\Http\Integrations\Docebo\Requests\DoceboLpsEnrollements;
-use App\Models\Enrollmodule;
-use App\Models\Learner;
 use App\Models\Lp;
-use App\Models\Lpenroll;
 use App\Models\Tenant;
+use App\Services\LpEnrollmentsService;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\DB;
 
 class UpdateEnrollementsLpsJob implements ShouldQueue
 {
@@ -37,75 +34,38 @@ class UpdateEnrollementsLpsJob implements ShouldQueue
     {
         $tenant = Tenant::find($this->tenantId);
         tenancy()->initialize($tenant);
+
+            // Initialize all neccessary Service
             $doceboConnector = new DoceboConnector();
+            $LpEnrollmentsService = new LpEnrollmentsService();
+
+            //Define Enrollments Fields
+            $fields = config('tenantconfigfields.enrollmentfields');
+            $enrollFields = $LpEnrollmentsService->getEnrollmentsFields($fields);
+
+            // GET Enrollements List DATA
             $lpsDoceboIds = Lp::pluck('docebo_id')->toArray();
             $request = new DoceboLpsEnrollements($lpsDoceboIds);
             $lpenrollsResponses = $doceboConnector->paginate($request);
-            $resultLps = [];
+
+            $results = [];
             foreach($lpenrollsResponses as $md){
                 $data = $md->dto();
-                $resultLps = array_merge($resultLps, $data);
+                $results = array_merge($results, $data);
             }
-            if(!empty($resultLps)){
-                $result = array_map(function ($item){
-                    $modulesIds = Lp::where('docebo_id' , $item['lp_docebo_id'])->first()->courses;
-                    $learner = Learner::where('docebo_id',$item['learner_docebo_id'])->first();
-                    if($learner){
-                        if($item['status'] != 'not_started')
-                        {
-                            $sumData = Enrollmodule::where('learner_docebo_id', $learner->docebo_id)
-                                    ->whereIn('module_docebo_id', $modulesIds)
-                                    ->selectRaw('SUM(session_time) as total_session_time')
-                                    ->selectRaw('SUM(cmi_time) as total_cmi_time')
-                                    ->selectRaw('SUM(calculated_time) as total_calculated_time')
-                                    ->selectRaw('SUM(recommended_time) as total_recommended_time')
-                                    ->first();
 
-                            $item['session_time'] = intval($sumData->total_session_time);
-                            $item['cmi_time'] = intval($sumData->total_cmi_time);
-                            $item['calculated_time'] = intval($sumData->total_calculated_time);
-                            $item['recommended_time'] = intval($sumData->total_recommended_time);
-
-                        }else{
-                            $item['session_time'] = 0;
-                            $item['cmi_time'] = 0;
-                            $item['calculated_time'] = 0;
-                            $item['recommended_time'] = 0;
-                        }
-
-                        $item['group_id'] = $learner->group->id;
-                        $item['project_id'] = $learner->project->id;
-                        return $item;
+            // BATCH insert DATA
+            if(!empty($results)){
+                $result = $LpEnrollmentsService->getEnrollmentsList($results, $fields);
+                if(count($result) > 1000)
+                {
+                    $batchData = array_chunk(array_filter($result), 1000);
+                    foreach($batchData as $data){
+                        $LpEnrollmentsService->batchInsert($data, $enrollFields);
                     }
-                }, $resultLps);
-                // Remove null values
-                $result = array_chunk(array_filter($result), 1000);
-                $upsertFunction = function ($chunk) {
-                    DB::transaction(function () use ($chunk) {
-                        Lpenroll::upsert(
-                            $chunk,
-                            [
-                                'learner_docebo_id',
-                                'lp_docebo_id',
-                            ],
-                            [
-                                'status',
-                                'enrollment_completion_percentage',
-                                'enrollment_created_at',
-                                'enrollment_updated_at',
-                                'enrollment_completed_at',
-                                'session_time',
-                                'cmi_time',
-                                'calculated_time',
-                                'recommended_time',
-                                'group_id',
-                                'project_id',
-                            ]
-                        );
-                    });
-                };
-                // Use array_map to apply the upsert function to each chunk
-                array_map($upsertFunction, $result);
+                }else{
+                    $LpEnrollmentsService->batchInsert($result, $enrollFields);
+                }
             }
         tenancy()->end();
     }
